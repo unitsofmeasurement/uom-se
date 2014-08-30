db76d44bf8a003c5c88d204cbf5ec75c2a6bbe01
otaviojava <otaviojava@java.net>
2014-08-30 09:51:27 -0300

    improve the test

--- src/test/java/tec/uom/se/quantity/QuantityTest.java
+++ src/test/java/tec/uom/se/quantity/QuantityTest.java
@@ -1,7 +1,11 @@
 package tec.uom.se.quantity;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import javax.measure.Quantity;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import tec.uom.se.AbstractQuantity;
@@ -13,8 +17,11 @@ public class QuantityTest {
 
     @Test
     public void toTest() {
-        Quantity len = AbstractQuantity.of(10, SI.METRE);
-        Quantity len2 = len.to(US.FOOT);
-        System.out.println(len2);
+        Quantity metre = AbstractQuantity.of(10, SI.METRE);
+        Quantity foot = metre.to(US.FOOT);
+        BigDecimal value = (BigDecimal) foot.getValue();
+        value.setScale(4, RoundingMode.HALF_EVEN);
+        BigDecimal expected = BigDecimal.valueOf(32.8084);
+        Assert.assertEquals(expected, value.setScale(4, RoundingMode.HALF_EVEN));
     }
 }
