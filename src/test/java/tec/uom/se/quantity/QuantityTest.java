64d84609490315a140a518ce979f62720f1b930f
otaviojava <otaviojava@java.net>
2014-08-31 12:15:16 -0300

    fixes issues 24

--- src/test/java/tec/uom/se/quantity/QuantityTest.java
+++ src/test/java/tec/uom/se/quantity/QuantityTest.java
@@ -3,6 +3,7 @@ package tec.uom.se.quantity;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import javax.measure.Quantity;
 import javax.measure.quantity.Length;
 
 import org.junit.Assert;
@@ -16,8 +17,8 @@ public class QuantityTest {
 
     @Test
     public void toTest() {
-        Length metre = (Length) AbstractQuantity.of(10, SI.METRE);
-        Length foot = (Length) metre.to(US.FOOT);
+        Quantity<Length> metre = AbstractQuantity.of(10, SI.METRE);
+        Quantity<Length> foot =  (Quantity<Length>) metre.to(US.FOOT);
         BigDecimal value = (BigDecimal) foot.getValue();
         value.setScale(4, RoundingMode.HALF_EVEN);
         BigDecimal expected = BigDecimal.valueOf(32.8084);
