e3798896161f1051187a47109816169bd99b589b
Werner Keil <werner.keil@gmx.net>
2014-09-06 23:53:41 +0200

    UNITSOFMEASUREMENT-44: Rename UnitConverter.getCompoundConverters() to getConversionSteps()
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-44

--- src/main/java/tec/uom/se/function/AbstractConverter.java
+++ src/main/java/tec/uom/se/function/AbstractConverter.java
@@ -86,7 +86,7 @@ public abstract class AbstractConverter implements UnitConverter {
     }
 
     @Override
-    public List<? extends UnitConverter> getCompoundConverters() {
+    public List<? extends UnitConverter> getConversionSteps() {
         List<AbstractConverter> converters = new ArrayList<>();
         converters.add(this);
         return converters;
@@ -189,10 +189,10 @@ public abstract class AbstractConverter implements UnitConverter {
         }
 
         @Override
-        public List<UnitConverter> getCompoundConverters() {
+        public List<UnitConverter> getConversionSteps() {
             List<UnitConverter> converters = new ArrayList<>();
-            List<? extends UnitConverter> leftCompound = left.getCompoundConverters();
-            List<? extends UnitConverter> rightCompound = right.getCompoundConverters();
+            List<? extends UnitConverter> leftCompound = left.getConversionSteps();
+            List<? extends UnitConverter> rightCompound = right.getConversionSteps();
             converters.addAll(leftCompound);
             converters.addAll(rightCompound);
             return converters;
