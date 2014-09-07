a6363d9f24617e29597990d1da36d5f94fe6402c
Werner Keil <werner.keil@gmx.net>
2014-09-07 17:29:40 +0200

    4: Add common module
    Task-Url: https://github.com/unitsofmeasurement/unit-lib/issues/issue/4

--- src/main/java/tec/uom/se/function/MaximumSupplier.java
+++ src/main/java/tec/uom/se/function/MaximumSupplier.java
@@ -1,9 +1,17 @@
 /**
- * Unit-API - Units of Measurement API for Java
- * Copyright (c) 2014 Jean-Marie Dautelle, Werner Keil, V2COM
- * All rights reserved.
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright 2013-2014, Jean-Marie Dautelle, Werner Keil, V2COM and individual
+ *  contributors by the @author tag.
  *
- * See LICENSE.txt for details.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package tec.uom.se.function;
 
