16d2f7e622efd3bc126a3d6afcd842bee5bf5c33
Werner Keil <werner.keil@gmx.net>
2015-04-19 23:01:51 +0200

    UNITSOFMEASUREMENT-107: Move some elements to a package "spi"
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-107

---
+++ src/main/java/tec/uom/se/spi/DefaultServiceProvider.java
@@ -0,0 +1,106 @@
+/**
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.uom.se.spi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.measure.spi.ServiceProvider;
+
+/**
+ * This class implements the (default) {@link ServiceProvider} interface and hereby uses the JDK
+ * {@link java.util.ServiceLoader} to load the services required.
+ *
+ * @author Werner Keil
+ */
+class DefaultServiceProvider implements ServiceProvider {
+    /** List of services loaded, per class. */
+    private final ConcurrentHashMap<Class, List<Object>> servicesLoaded = new ConcurrentHashMap<Class, List<Object>>();
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    /**
+     * Loads and registers services.
+     *
+     * @param serviceType
+     *            The service type.
+     * @param <T>
+     *            the concrete type.
+     * @return the items found, never {@code null}.
+     */
+    @Override
+    public <T> List<T> getServices(final Class<T> serviceType) {
+        @SuppressWarnings("unchecked")
+        List<T> found = (List<T>) servicesLoaded.get(serviceType);
+        if (found != null) {
+            return found;
+        }
+
+        return loadServices(serviceType);
+    }
+
+    @Override
+    public <T> T getService(Class<T> serviceType) {
+        List<T> servicesFound = getServices(serviceType);
+        if(servicesFound.isEmpty()){
+            return null;
+        }
+        return servicesFound.get(0);
+    }
+
+    /**
+     * Loads and registers services.
+     *
+     * @param   serviceType  The service type.
+     * @param   <T>          the concrete type.
+     *
+     * @return  the items found, never {@code null}.
+     */
+    private <T> List<T> loadServices(final Class<T> serviceType) {
+        List<T> services = new ArrayList<T>();
+        try {
+            for (T t : ServiceLoader.load(serviceType)) {
+                services.add(t);
+            }
+            @SuppressWarnings("unchecked")
+            final List<T> previousServices = (List<T>) servicesLoaded.putIfAbsent(serviceType, (List<Object>) services);
+            return Collections.unmodifiableList(previousServices != null ? previousServices : services);
+        } catch (Exception e) {
+            Logger.getLogger(DefaultServiceProvider.class.getName()).log(Level.WARNING,
+                                                                         "Error loading services of type " + serviceType, e);
+            return services;
+        }
+    }
+
+}
