5d23947a1930ca48dbffd940dc9893538f25e4f8
Werner Keil <werner.keil@gmx.net>
2015-06-24 23:21:54 +0200

    4: SI.GRAM implementation problem
    Task-Url:
    https://github.com/unitsofmeasurement/si-units/issues/issues/issue/4

--- src/test/java/tec/uom/se/unit/PrefixTest.java
+++ src/test/java/tec/uom/se/unit/PrefixTest.java
@@ -30,11 +30,18 @@
 package tec.uom.se.unit;
 
 import static org.junit.Assert.assertEquals;
-import static tec.uom.se.unit.MetricPrefix.KILO;
+import static tec.uom.se.unit.MetricPrefix.*;
 import static tec.uom.se.unit.SI.GRAM;
 import static tec.uom.se.unit.SI.KILOGRAM;
 
+import javax.measure.Quantity;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Mass;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
+import tec.uom.se.quantity.Quantities;
 
 public class PrefixTest {
 	@Test
@@ -42,5 +49,39 @@ public class PrefixTest {
 		// TODO how to handle equals for units?
 		assertEquals(KILOGRAM.getSymbol(), KILO(GRAM).getSymbol());
 		assertEquals(KILOGRAM.toString(), KILO(GRAM).toString());
+	}
+	
+	@Test
+	public void testMicro2() {
+		Quantity<Length> m1 = Quantities.getQuantity(1.0, SI.METRE);
+		assertEquals(1d, m1.getValue());
+		assertEquals("m", m1.getUnit().toString());
+				
+		Quantity<Length> m2 = m1.to(MICRO(SI.METRE));
+		assertEquals(1000000.0d, m2.getValue());
+		assertEquals("µm", m2.getUnit().toString());
+	}
+	
+	@Test
+	@Ignore
+	public void testNano() {
+		Quantity<Mass> m1 = Quantities.getQuantity(1.0, SI.GRAM);
+		assertEquals(1d, m1.getValue());
+		assertEquals("g", m1.getUnit().toString());
+				
+		Quantity<Mass> m2 = m1.to(NANO(SI.GRAM));
+		assertEquals(1000000000.0d, m2.getValue());
+		assertEquals("ng", m2.getUnit().toString());
+	}
+	
+	@Test
+	public void testNano2() {
+		Quantity<Length> m1 = Quantities.getQuantity(1.0, SI.METRE);
+		assertEquals(1d, m1.getValue());
+		assertEquals("m", m1.getUnit().toString());
+				
+		Quantity<Length> m2 = m1.to(NANO(SI.METRE));
+		assertEquals(1000000000.0d, m2.getValue());
+		assertEquals("nm", m2.getUnit().toString());
 	}
 }
