2b23704fb189e68788f8dcf16a184b88823f6354
Werner Keil <werner.keil@gmx.net>
2015-07-09 23:20:15 +0200

    UNITSOFMEASUREMENT-21: Portability and Modularity
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-21

---
+++ src/main/java/tec/uom/se/spi/PriorityAwareServiceProvider.java
@@ -0,0 +1,141 @@
+/**
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.uom.se.spi;
+
+import javax.annotation.Priority;
+import javax.measure.spi.ServiceProvider;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class implements the (default) {@link javax.measure.spi.ServiceProvider} interface and hereby uses the JDK
+ * {@link java.util.ServiceLoader} to load the services required.
+ *
+ * @author Werner Keil
+ */
+public class PriorityAwareServiceProvider implements ServiceProvider {
+    /**
+     * List of services loaded, per class.
+     */
+    private final ConcurrentHashMap<Class, List<Object>> servicesLoaded = new ConcurrentHashMap<>();
+
+    private static final Comparator<Object> SERVICE_COMPARATOR = new Comparator<Object>(){
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            return PriorityAwareServiceProvider.compareServices(o1, o2);
+        }
+    };
+
+    /**
+     * Returns a prioritx value of 10.
+     *
+     * @return 10, overriding the default provider.
+     */
+    @Override
+    public int getPriority() {
+        return 10;
+    }
+
+    /**
+     * Loads and registers services.
+     *
+     * @param serviceType The service type.
+     * @param <T>         the concrete type.
+     * @return the items found, never {@code null}.
+     */
+    @Override
+    public <T> List<T> getServices(final Class<T> serviceType) {
+        @SuppressWarnings("unchecked")
+        List<T> found = (List<T>) servicesLoaded.get(serviceType);
+        if (found != null) {
+            return found;
+        }
+
+        return loadServices(serviceType);
+    }
+
+    @Override
+    public <T> T getService(Class<T> serviceType) {
+        List<T> services = getServices(serviceType);
+        if(services.isEmpty()){
+            return null;
+        }
+        return services.get(0);
+    }
+
+    public static int compareServices(Object o1, Object o2) {
+        int prio1 = 0;
+        int prio2 = 0;
+        Priority prio1Annot = o1.getClass().getAnnotation(Priority.class);
+        if (prio1Annot != null) {
+            prio1 = prio1Annot.value();
+        }
+        Priority prio2Annot = o2.getClass().getAnnotation(Priority.class);
+        if (prio2Annot != null) {
+            prio2 = prio2Annot.value();
+        }
+        if (prio1 < prio2) {
+            return 1;
+        }
+        if (prio2 < prio1) {
+            return -1;
+        }
+        return o2.getClass().getSimpleName().compareTo(o1.getClass().getSimpleName());
+    }
+
+    /**
+     * Loads and registers services.
+     *
+     * @param serviceType The service type.
+     * @param <T>         the concrete type.
+     * @return the items found, never {@code null}.
+     */
+    private <T> List<T> loadServices(final Class<T> serviceType) {
+        List<T> services = new ArrayList<>();
+        try {
+            for (T t : ServiceLoader.load(serviceType)) {
+                services.add(t);
+            }
+            Collections.sort(services, SERVICE_COMPARATOR);
+            @SuppressWarnings("unchecked")
+            final List<T> previousServices = (List<T>) servicesLoaded.putIfAbsent(serviceType, (List<Object>) services);
+            return Collections.unmodifiableList(previousServices != null ? previousServices : services);
+        } catch (Exception e) {
+            Logger.getLogger(PriorityAwareServiceProvider.class.getName()).log(Level.WARNING,
+                    "Error loading services of type " + serviceType, e);
+            Collections.sort(services, SERVICE_COMPARATOR);
+            return services;
+        }
+    }
+
+}
