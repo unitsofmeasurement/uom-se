f62e2e7aeff75dfd4641625fb0407e790abe3d5d
Werner Keil <werner.keil@gmx.net>
2015-09-27 18:21:28 +0200

    UNITSOFMEASUREMENT-165: Add getSystemUnit() method to QuantityFactory
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-165

--- src/main/java/tec/uom/se/quantity/DefaultQuantityFactory.java
+++ src/main/java/tec/uom/se/quantity/DefaultQuantityFactory.java
@@ -29,10 +29,15 @@
  */
 package tec.uom.se.quantity;
 
+import static tec.uom.se.unit.Units.*;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import javax.measure.Quantity;
 import javax.measure.Unit;
+import javax.measure.quantity.*;
 import javax.measure.spi.QuantityFactory;
 
 /**
@@ -48,34 +53,86 @@ import javax.measure.spi.QuantityFactory;
  * @author  <a href="mailto:units@catmedia.us">Werner Keil</a>
  * @author  <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:otaviojava@java.net">Otavio Santana</a>
- * @version 0.7, $Date: 2015-07-25 $
+ * @version 0.7.1, $Date: 2015-09-27 $
  */
 public class DefaultQuantityFactory <Q extends Quantity<Q>> implements QuantityFactory<Q>{
-    private final Class<Q> quantity;
-
-    public DefaultQuantityFactory(Class<Q> quantity) {
-        this.quantity = quantity;
+	/**
+	 * The type of the quantities created by this factory.
+	 */
+	private final Class<Q> type;
+    
+	/**
+	 * The metric unit for quantities created by this factory.
+	 */
+	private final Unit<Q> metricUnit;
+	
+	@SuppressWarnings("rawtypes")
+	static final Map<Class, Unit> CLASS_TO_METRIC_UNIT = new HashMap<Class, Unit>();
+	static {
+		CLASS_TO_METRIC_UNIT.put(Dimensionless.class, ONE);
+		CLASS_TO_METRIC_UNIT.put(ElectricCurrent.class, AMPERE);
+		CLASS_TO_METRIC_UNIT.put(LuminousIntensity.class, CANDELA);
+		CLASS_TO_METRIC_UNIT.put(Temperature.class, KELVIN);
+		CLASS_TO_METRIC_UNIT.put(Mass.class, KILOGRAM);
+		CLASS_TO_METRIC_UNIT.put(Length.class, METRE);
+		CLASS_TO_METRIC_UNIT.put(AmountOfSubstance.class, MOLE);
+		CLASS_TO_METRIC_UNIT.put(Time.class, SECOND);
+		CLASS_TO_METRIC_UNIT.put(Angle.class, RADIAN);
+		CLASS_TO_METRIC_UNIT.put(SolidAngle.class, STERADIAN);
+		CLASS_TO_METRIC_UNIT.put(Frequency.class, HERTZ);
+		CLASS_TO_METRIC_UNIT.put(Force.class, NEWTON);
+		CLASS_TO_METRIC_UNIT.put(Pressure.class, PASCAL);
+		CLASS_TO_METRIC_UNIT.put(Energy.class, JOULE);
+		CLASS_TO_METRIC_UNIT.put(Power.class, WATT);
+		CLASS_TO_METRIC_UNIT.put(ElectricCharge.class, COULOMB);
+		CLASS_TO_METRIC_UNIT.put(ElectricPotential.class, VOLT);
+		CLASS_TO_METRIC_UNIT.put(ElectricCapacitance.class, FARAD);
+		CLASS_TO_METRIC_UNIT.put(ElectricResistance.class, OHM);
+		CLASS_TO_METRIC_UNIT.put(ElectricConductance.class, SIEMENS);
+		CLASS_TO_METRIC_UNIT.put(MagneticFlux.class, WEBER);
+		CLASS_TO_METRIC_UNIT.put(MagneticFluxDensity.class, TESLA);
+		CLASS_TO_METRIC_UNIT.put(ElectricInductance.class, HENRY);
+		CLASS_TO_METRIC_UNIT.put(LuminousFlux.class, LUMEN);
+		CLASS_TO_METRIC_UNIT.put(Illuminance.class, LUX);
+		CLASS_TO_METRIC_UNIT.put(Radioactivity.class, BECQUEREL);
+		CLASS_TO_METRIC_UNIT.put(RadiationDoseAbsorbed.class, GRAY);
+		CLASS_TO_METRIC_UNIT.put(RadiationDoseEffective.class, SIEVERT);
+		CLASS_TO_METRIC_UNIT.put(CatalyticActivity.class, KATAL);
+		CLASS_TO_METRIC_UNIT.put(Speed.class, METRES_PER_SECOND);
+		CLASS_TO_METRIC_UNIT.put(Acceleration.class, METRES_PER_SQUARE_SECOND);
+		CLASS_TO_METRIC_UNIT.put(Area.class, SQUARE_METRE);
+		CLASS_TO_METRIC_UNIT.put(Volume.class, CUBIC_METRE);
+	}
+    
+    @SuppressWarnings("unchecked")
+	public DefaultQuantityFactory(Class<Q> quantity) {
+        type = quantity;
+        metricUnit = CLASS_TO_METRIC_UNIT.get(type);
     }
 
     public String toString() {
-        return "tec.uom.se.DefaultQuantityFactory <" + quantity.getName() + '>';
+        return "tec.uom.se.DefaultQuantityFactory <" + type.getName() + '>';
     }
 
     public boolean equals(Object obj) {
         if(DefaultQuantityFactory.class.isInstance(obj)) {
             @SuppressWarnings("rawtypes")
             DefaultQuantityFactory other = DefaultQuantityFactory.class.cast(obj);
-            return Objects.equals(quantity, other.quantity);
+            return Objects.equals(type, other.type);
         }
         return false;
     }
 
     public int hashCode() {
-        return quantity.hashCode();
+        return type.hashCode();
     }
 
     @SuppressWarnings("unchecked")
     public Quantity<Q> create(Number value, Unit<Q> unit) {
         return (Q) tec.uom.se.quantity.Quantities.getQuantity(value, unit);
     }
+    
+	public Unit<Q> getSystemUnit() {
+		return metricUnit;
+	}
 }
