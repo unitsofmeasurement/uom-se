dd4b312a8bc19ace8c5650a81e42dae3fb2235e5
Werner Keil <werner.keil@gmx.net>
2017-07-10 01:24:41 +0200

    3: Verify UCUM prefix parsing
    Task-Url: https://github.com/unitsofmeasurement/jackson-module-unitsofmeasure/issues/issues/3

--- src/main/java/tec/uom/se/format/EBNFUnitFormat.java
+++ src/main/java/tec/uom/se/format/EBNFUnitFormat.java
@@ -34,6 +34,8 @@ import tec.uom.se.internal.format.TokenException;
 import tec.uom.se.internal.format.TokenMgrError;
 import tec.uom.se.internal.format.UnitFormatParser;
 import tec.uom.se.unit.AnnotatedUnit;
+
+import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.measure.format.ParserException;
 
@@ -50,8 +52,9 @@ import java.util.ResourceBundle;
  * 
  * <h3>Here is the grammar for Units in Extended Backus-Naur Form (EBNF)</h3>
  * <p>
- * Note that the grammar has been left-factored to be suitable for use by a top-down parser generator such as <a
- * href="https://javacc.dev.java.net/">JavaCC</a>
+ * Note that the grammar has been left-factored to be suitable for use by a
+ * top-down parser generator such as
+ * <a href="https://javacc.dev.java.net/">JavaCC</a>
  * </p>
  * <table width="90%" align="center">
  * <tr>
@@ -80,21 +83,26 @@ import java.util.ResourceBundle;
  * <tr valign="top">
  * <td>&lt;number&gt;</td>
  * <td>:=</td>
- * <td>(&lt;sign&gt;)? (&lt;digit&gt;)* (".")? (&lt;digit&gt;)+ (("e" | "E") (&lt;sign&gt;)? (&lt;digit&gt;)+)?</td>
+ * <td>(&lt;sign&gt;)? (&lt;digit&gt;)* (".")? (&lt;digit&gt;)+ (("e" | "E")
+ * (&lt;sign&gt;)? (&lt;digit&gt;)+)?</td>
  * </tr>
  * <tr valign="top">
  * <td>&lt;exponent&gt;</td>
  * <td>:=</td>
  * <td>( "^" ( &lt;sign&gt; )? &lt;integer&gt; ) <br>
- * | ( "^(" (&lt;sign&gt;)? &lt;integer&gt; ( "/" (&lt;sign&gt;)? &lt;integer&gt; )? ")" ) <br>
+ * | ( "^(" (&lt;sign&gt;)? &lt;integer&gt; ( "/" (&lt;sign&gt;)?
+ * &lt;integer&gt; )? ")" ) <br>
  * | ( &lt;superscript_digit&gt; )+</td>
  * </tr>
  * <tr valign="top">
  * <td>&lt;initial_char&gt;</td>
  * <td>:=</td>
- * <td>? Any Unicode character excluding the following: ASCII control & whitespace (&#92;u0000 - &#92;u0020), decimal digits '0'-'9', '('
- * (&#92;u0028), ')' (&#92;u0029), '*' (&#92;u002A), '+' (&#92;u002B), '-' (&#92;u002D), '.' (&#92;u002E), '/' (&#92;u005C), ':' (&#92;u003A), '^'
- * (&#92;u005E), '²' (&#92;u00B2), '³' (&#92;u00B3), '·' (&#92;u00B7), '¹' (&#92;u00B9), '⁰' (&#92;u2070), '⁴' (&#92;u2074), '⁵' (&#92;u2075), '⁶'
+ * <td>? Any Unicode character excluding the following: ASCII control &
+ * whitespace (&#92;u0000 - &#92;u0020), decimal digits '0'-'9', '('
+ * (&#92;u0028), ')' (&#92;u0029), '*' (&#92;u002A), '+' (&#92;u002B), '-'
+ * (&#92;u002D), '.' (&#92;u002E), '/' (&#92;u005C), ':' (&#92;u003A), '^'
+ * (&#92;u005E), '²' (&#92;u00B2), '³' (&#92;u00B3), '·' (&#92;u00B7), '¹'
+ * (&#92;u00B9), '⁰' (&#92;u2070), '⁴' (&#92;u2074), '⁵' (&#92;u2075), '⁶'
  * (&#92;u2076), '⁷' (&#92;u2077), '⁸' (&#92;u2078), '⁹' (&#92;u2079) ?</td>
  * </tr>
  * <tr valign="top">
@@ -118,12 +126,14 @@ import java.util.ResourceBundle;
  * <tr valign="top">
  * <td>&lt;add_expr&gt;</td>
  * <td>:=</td>
- * <td>( &lt;number&gt; &lt;sign&gt; )? &lt;mul_expr&gt; ( &lt;sign&gt; &lt;number&gt; )?</td>
+ * <td>( &lt;number&gt; &lt;sign&gt; )? &lt;mul_expr&gt; ( &lt;sign&gt;
+ * &lt;number&gt; )?</td>
  * </tr>
  * <tr valign="top">
  * <td>&lt;mul_expr&gt;</td>
  * <td>:=</td>
- * <td>&lt;exponent_expr&gt; ( ( ( "*" | "·" ) &lt;exponent_expr&gt; ) | ( "/" &lt;exponent_expr&gt; ) )*</td>
+ * <td>&lt;exponent_expr&gt; ( ( ( "*" | "·" ) &lt;exponent_expr&gt; ) | ( "/"
+ * &lt;exponent_expr&gt; ) )*</td>
  * </tr>
  * <tr valign="top">
  * <td>&lt;exponent_expr&gt;</td>
@@ -148,134 +158,140 @@ import java.util.ResourceBundle;
  */
 public class EBNFUnitFormat extends AbstractUnitFormat {
 
-  // ////////////////////////////////////////////////////
-  // Class variables //
-  // ////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////
+    // Class variables //
+    // ////////////////////////////////////////////////////
 
-  /**
+    /**
     * 
     */
-  // private static final long serialVersionUID = 8968559300292910840L;
+    // private static final long serialVersionUID = 8968559300292910840L;
 
-  /**
-   * Name of the resource bundle
-   */
-  private static final String BUNDLE_NAME = "tec.uom.se.format.messages"; //$NON-NLS-1$
+    /**
+     * Name of the resource bundle
+     */
+    private static final String BUNDLE_NAME = "tec.uom.se.format.messages"; //$NON-NLS-1$
 
-  /**
-   * Default locale instance. If the default locale is changed after the class is initialized, this instance will no longer be used.
-   */
-  private static final EBNFUnitFormat DEFAULT_INSTANCE = new EBNFUnitFormat();
+    /**
+     * Default locale instance. If the default locale is changed after the class
+     * is initialized, this instance will no longer be used.
+     */
+    private static final EBNFUnitFormat DEFAULT_INSTANCE = new EBNFUnitFormat();
 
-  /**
-   * Returns the instance for the current default locale (non-ascii characters are allowed)
-   */
-  public static EBNFUnitFormat getInstance() {
-    return DEFAULT_INSTANCE;
-  }
-
-  /** Returns an instance for the given symbol map. */
-  public static EBNFUnitFormat getInstance(SymbolMap symbols) {
-    return new EBNFUnitFormat(symbols);
-  }
-
-  // //////////////////////
-  // Instance variables //
-  // //////////////////////
-  /**
-   * The symbol map used by this instance to map between {@link org.unitsofmeasure.Unit Unit}s and <code>String</code>s, etc...
-   */
-  private final transient SymbolMap symbolMap;
-
-  // ////////////////
-  // Constructors //
-  // ////////////////
-  /**
-   * Base constructor.
-   * 
-   */
-  EBNFUnitFormat() {
-    this(SymbolMap.of(ResourceBundle.getBundle(BUNDLE_NAME, Locale.ROOT)));
-  }
-
-  /**
-   * Private constructor.
-   * 
-   * @param symbols
-   *          the symbol mapping.
-   */
-  private EBNFUnitFormat(SymbolMap symbols) {
-    symbolMap = symbols;
-  }
-
-  // //////////////////////
-  // Instance methods //
-  // //////////////////////
-  /**
-   * Get the symbol map used by this instance to map between {@link org.unitsofmeasure.Unit Unit}s and <code>String</code>s, etc...
-   * 
-   * @return SymbolMap the current symbol map
-   */
-  protected SymbolMap getSymbols() {
-    return symbolMap;
-  }
-
-  // //////////////
-  // Formatting //
-  // //////////////
-  public Appendable format(Unit<?> unit, Appendable appendable) throws IOException {
-
-    EBNFHelper.formatInternal(unit, appendable, symbolMap);
-    if (unit instanceof AnnotatedUnit<?>) {
-      AnnotatedUnit<?> annotatedUnit = (AnnotatedUnit<?>) unit;
-      if (annotatedUnit.getAnnotation() != null) {
-        appendable.append('{');
-        appendable.append(annotatedUnit.getAnnotation());
-        appendable.append('}');
-      }
+    /**
+     * Returns the instance for the current default locale (non-ascii characters
+     * are allowed)
+     */
+    public static EBNFUnitFormat getInstance() {
+	return DEFAULT_INSTANCE;
     }
-    return appendable;
-  }
 
-  public boolean isLocaleSensitive() {
-    return false;
-  }
-
-  protected Unit<?> parse(CharSequence csq, int index) throws IllegalArgumentException {
-    // Parsing reads the whole character sequence from the parse position.
-    int start = index; // cursor != null ? cursor.getIndex() : 0;
-    int end = csq.length();
-    if (end <= start) {
-      return AbstractUnit.ONE;
+    /** Returns an instance for the given symbol map. */
+    public static EBNFUnitFormat getInstance(SymbolMap symbols) {
+	return new EBNFUnitFormat(symbols);
     }
-    String source = csq.subSequence(start, end).toString().trim();
-    if (source.length() == 0) {
-      return AbstractUnit.ONE;
-    }
-    try {
-      UnitFormatParser parser = new UnitFormatParser(symbolMap, new StringReader(source));
-      Unit<?> result = parser.parseUnit();
-      // if (cursor != null)
-      // cursor.setIndex(end);
-      return result;
-    } catch (TokenException | TokenMgrError e) {
-      // if (cursor != null) {
-      // if (e.currentToken != null) {
-      // cursor.setErrorIndex(start + e.currentToken.endColumn);
-      // } else {
-      // cursor.setErrorIndex(start);
-      // }
-      // }
-      throw new ParserException(e);
-    }
-  }
 
-  public Unit<?> parse(CharSequence csq) throws ParserException {
-    return parse(csq, 0);
-  }
+    // //////////////////////
+    // Instance variables //
+    // //////////////////////
+    /**
+     * The symbol map used by this instance to map between
+     * {@link org.unitsofmeasure.Unit Unit}s and <code>String</code>s, etc...
+     */
+    private final transient SymbolMap symbolMap;
 
-  @Override
-  protected Unit<?> parse(CharSequence csq, ParsePosition cursor) throws IllegalArgumentException {
-    return parse(csq);
-  }
+    // ////////////////
+    // Constructors //
+    // ////////////////
+    /**
+     * Base constructor.
+     * 
+     */
+    EBNFUnitFormat() {
+	this(SymbolMap.of(ResourceBundle.getBundle(BUNDLE_NAME, Locale.ROOT)));
+    }
+
+    /**
+     * Private constructor.
+     * 
+     * @param symbols
+     *            the symbol mapping.
+     */
+    private EBNFUnitFormat(SymbolMap symbols) {
+	symbolMap = symbols;
+    }
+
+    // //////////////////////
+    // Instance methods //
+    // //////////////////////
+    /**
+     * Get the symbol map used by this instance to map between
+     * {@link org.unitsofmeasure.Unit Unit}s and <code>String</code>s, etc...
+     * 
+     * @return SymbolMap the current symbol map
+     */
+    protected SymbolMap getSymbols() {
+	return symbolMap;
+    }
+
+    // //////////////
+    // Formatting //
+    // //////////////
+    public Appendable format(Unit<?> unit, Appendable appendable) throws IOException {
+
+	EBNFHelper.formatInternal(unit, appendable, symbolMap);
+	if (unit instanceof AnnotatedUnit<?>) {
+	    AnnotatedUnit<?> annotatedUnit = (AnnotatedUnit<?>) unit;
+	    if (annotatedUnit.getAnnotation() != null) {
+		appendable.append('{');
+		appendable.append(annotatedUnit.getAnnotation());
+		appendable.append('}');
+	    }
+	}
+	return appendable;
+    }
+
+    public boolean isLocaleSensitive() {
+	return false;
+    }
+
+    @Override
+    protected Unit<? extends Quantity<?>> parse(CharSequence csq, ParsePosition cursor) throws ParserException {
+	// Parsing reads the whole character sequence from the parse position.
+	int start = cursor != null ? cursor.getIndex() : 0;
+	int end = csq.length();
+	if (end <= start) {
+	    return AbstractUnit.ONE;
+	}
+	String source = csq.subSequence(start, end).toString().trim();
+	if (source.length() == 0) {
+	    return AbstractUnit.ONE;
+	}
+	try {
+	    UnitFormatParser parser = new UnitFormatParser(symbolMap, new StringReader(source));
+	    Unit<?> result = parser.parseUnit();
+	    if (cursor != null)
+		cursor.setIndex(end);
+	    return result;
+	} catch (TokenException e) {
+	    if (e.currentToken != null) {
+		cursor.setErrorIndex(start + e.currentToken.endColumn);
+	    } else {
+		cursor.setErrorIndex(start);
+	    }
+	    throw new ParserException(e);
+	} catch (TokenMgrError e) {
+	    cursor.setErrorIndex(start);
+	    throw new IllegalArgumentException(e.getMessage());
+	}
+    }
+
+    @Override
+    protected Unit<?> parse(CharSequence csq, int index) throws IllegalArgumentException {
+	return parse(csq, new ParsePosition(index));
+    }
+
+    public Unit<?> parse(CharSequence csq) throws ParserException {
+	return parse(csq, 0);
+    }
 }
