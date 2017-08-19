64ff92d8a8f270edaa7d86c16f1dc13de3c41e1f
Chris Hennick <seahen123@gmail.com>
2017-08-19 02:33:03 +0000

    Explicit visibility for ProductUnit.getPowInstance (advised by Codacy)

--- src/main/java/tec/uom/se/unit/ProductUnit.java
+++ src/main/java/tec/uom/se/unit/ProductUnit.java
@@ -198,7 +198,7 @@ public final class ProductUnit<Q extends Quantity<Q>> extends AbstractUnit<Q> {
    *          the exponent (nn &gt; 0).
    * @return <code>unit^n</code>
    */
-  static AbstractUnit<?> getPowInstance(Unit<?> unit, int n) {
+  public static AbstractUnit<?> getPowInstance(Unit<?> unit, int n) {
     Element[] unitElems;
     if (unit instanceof ProductUnit<?>) {
       Element[] elems = ((ProductUnit<?>) unit).elements;
